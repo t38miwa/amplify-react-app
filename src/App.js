5c8cb35464392f2d9ab5d5913d435124196b6511
t38miwa <t38miwa@gmail.com>
2024-01-28 11:52:42 +0900

    added auth-4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App({ signOut }) {
   return (
     <View className="App">
       <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
+        <Image src={logo} className="App-logo" alt="logo-react" />
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
